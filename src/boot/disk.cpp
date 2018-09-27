d8fca46926d3af2890b2b520573c499a81235fcb
Petr Volf <petr.volf96@gmail.com>
2018-09-27 13:39:48 +0200

    init

---
+++ src/boot/disk.cpp
@@ -0,0 +1,143 @@
+//Tento kod nebyl odladen. Mozna, ze tu budou chyby k odhaleni... a mozna i tak k ziskani nejakeho toho bonusoveho bodu.
+
+#include "disk.h"
+
+#include <array>
+
+std::array<std::unique_ptr<CDisk_Drive>, 256> disk_drives;
+
+#undef max
+
+void __stdcall Disk_Handler(kiv_hal::TRegisters &context) {
+	if (!disk_drives[context.rdx.l]) {
+		auto cmos_params = cmos.Drive_Parameters(context.rdx.l);
+		if (cmos_params.is_present) {
+			if (cmos_params.is_ram_disk) disk_drives[context.rdx.l].reset(new CRAM_Disk{ cmos_params });
+				else disk_drives[context.rdx.l].reset(new CDisk_Image{ cmos_params });
+		}
+		else {
+			context.flags.carry = 1;
+			context.rax.x = static_cast<uint16_t>(kiv_hal::NDisk_Status::Drive_Not_Ready);
+		}
+	}
+
+	switch (static_cast<kiv_hal::NDisk_IO>(context.rax.h)) {		
+		case kiv_hal::NDisk_IO::Read_Sectors:		return disk_drives[context.rdx.l]->Read_Sectors(context);
+		case kiv_hal::NDisk_IO::Write_Sectors:		return disk_drives[context.rdx.l]->Write_Sectors(context);
+		case kiv_hal::NDisk_IO::Drive_Parameters:	return disk_drives[context.rdx.l]->Drive_Parameters(context);
+
+		default: context.flags.carry = 1;
+				 context.rax.x = static_cast<uint16_t>(kiv_hal::NDisk_Status::Bad_Command);
+	}
+}
+
+CDisk_Drive::CDisk_Drive(const TCMOS_Drive_Parameters &cmos_parameters) : mBytes_Per_Sector(cmos_parameters.bytes_per_sector), mDisk_Size(0) {
+
+}
+
+void CDisk_Drive::Set_Status(kiv_hal::TRegisters &context, const kiv_hal::NDisk_Status status) {
+	context.flags.carry = status == kiv_hal::NDisk_Status::No_Error ? 0 : 1;
+	if (context.flags.carry) context.rax.x = static_cast<uint16_t>(status);
+}
+
+void CDisk_Drive::Drive_Parameters(kiv_hal::TRegisters &context) {
+	kiv_hal::TDrive_Parameters &params = *reinterpret_cast<kiv_hal::TDrive_Parameters*>(context.rdi.r);
+
+	const size_t MB = 1024 * 1024; //1MiB
+	bool assisted_translation = true;
+	
+	if (mDisk_Size < 504 * MB) params.heads = 16;
+	 else  if (mDisk_Size < 1008 * MB) params.heads = 32;
+		else if (mDisk_Size < 2016 * MB) params.heads = 64;
+			else if (mDisk_Size < 4032 * MB) params.heads = 128;
+				else if (mDisk_Size < 8032 * MB) params.heads = 255;
+				else
+					//pro tyhle cisla nemame standardni prepocet, tak nastavime vse na max
+					{
+						params.sectors_per_track = std::numeric_limits<decltype(params.sectors_per_track)>::max();
+						params.heads = std::numeric_limits<decltype(params.heads)>::max();
+						params.cylinders = std::numeric_limits<decltype(params.cylinders)>::max();
+						assisted_translation = false;
+					};
+
+	if (assisted_translation) {
+		params.sectors_per_track = 63;
+//		size_t tmp1 = static_cast<size_t>(params.sectors_per_track * params.heads);
+		//size_t tmp2 = tmp1 * mBytes_Per_Sector;
+		//tmp2 /= mDisk_Size;
+		params.cylinders = static_cast<decltype(params.cylinders) > (mDisk_Size / (static_cast<size_t>(params.sectors_per_track) * static_cast<size_t>(params.heads) * mBytes_Per_Sector));
+		//params.cylinders = static_cast<decltype(params.cylinders)>(tmp2);
+	}
+
+	params.bytes_per_sector = static_cast<decltype(params.bytes_per_sector)>(mBytes_Per_Sector);
+	params.absolute_number_of_sectors = mDisk_Size / mBytes_Per_Sector;
+	Set_Status(context, kiv_hal::NDisk_Status::No_Error);
+}
+
+
+bool CDisk_Drive::Check_DAP(kiv_hal::TRegisters &context) {
+	kiv_hal::TDisk_Address_Packet &dap = *reinterpret_cast<kiv_hal::TDisk_Address_Packet*>(context.rdi.r);
+
+	if (mBytes_Per_Sector*(dap.lba_index + dap.count) >= mDisk_Size) {
+		//nemuzeme dovolit, vysledkem by byl pristup za velikost disku
+		Set_Status(context, kiv_hal::NDisk_Status::Sector_Not_Found);
+		return false;
+	}
+
+	return true;
+}
+
+CDisk_Image::CDisk_Image(const TCMOS_Drive_Parameters &cmos_parameters) : CDisk_Drive(cmos_parameters) {
+	auto open_mode = std::ios::binary | std::ios::in;
+	if (!cmos_parameters.read_only) open_mode |= std::ios::out;
+	mDisk_Image.open(cmos_parameters.disk_image, open_mode);	
+	mDisk_Image.seekg(0, std::ios::end);      //rovnou nastavime pozici na konce souboru, protoze pri zapisu ji budeme stejne prestavovat
+	mDisk_Size = mDisk_Image.tellg();
+}
+
+
+void CDisk_Image::Read_Sectors(kiv_hal::TRegisters &context) {
+	if (Check_DAP(context)) {
+		kiv_hal::TDisk_Address_Packet &dap = *reinterpret_cast<kiv_hal::TDisk_Address_Packet*>(context.rdi.r);
+		mDisk_Image.seekg(mBytes_Per_Sector*dap.lba_index, std::ios::beg);
+		const auto bytes_to_read = dap.count*mBytes_Per_Sector;
+		mDisk_Image.read(static_cast<char*>(dap.sectors), bytes_to_read);
+		if (mDisk_Image.gcount() == bytes_to_read) Set_Status(context, kiv_hal::NDisk_Status::No_Error);
+			else Set_Status(context, kiv_hal::NDisk_Status::Address_Mark_Not_Found_Or_Bad_Sector);
+	}
+}
+
+void CDisk_Image::Write_Sectors(kiv_hal::TRegisters &context) {
+	if (Check_DAP(context)) {
+		kiv_hal::TDisk_Address_Packet &dap = *reinterpret_cast<kiv_hal::TDisk_Address_Packet*>(context.rdi.r);
+		mDisk_Image.seekg(mBytes_Per_Sector*dap.lba_index, std::ios::beg);
+		const auto bytes_to_write = dap.count*mBytes_Per_Sector;
+		mDisk_Image.write(static_cast<char*>(dap.sectors), bytes_to_write);
+		if (mDisk_Image.gcount() == bytes_to_write) Set_Status(context, kiv_hal::NDisk_Status::No_Error);
+			else Set_Status(context, kiv_hal::NDisk_Status::Fixed_Disk_Write_Fault_On_Selected_Drive);
+	}
+}
+
+CRAM_Disk::CRAM_Disk(const TCMOS_Drive_Parameters &cmos_parameters) : CDisk_Drive(cmos_parameters) {
+	mDisk_Size = cmos_parameters.RAM_Disk_Size;
+	mDisk_Image.resize(mDisk_Size);
+}
+
+
+void CRAM_Disk::Read_Sectors(kiv_hal::TRegisters &context) {
+	if (Check_DAP(context)) {
+		kiv_hal::TDisk_Address_Packet &dap = *reinterpret_cast<kiv_hal::TDisk_Address_Packet*>(context.rdi.r);
+
+		memcpy(dap.sectors, &mDisk_Image[dap.lba_index*mBytes_Per_Sector], mBytes_Per_Sector*dap.count);
+		Set_Status(context, kiv_hal::NDisk_Status::No_Error);
+	}
+}
+
+void CRAM_Disk::Write_Sectors(kiv_hal::TRegisters &context) {
+	if (Check_DAP(context)) {
+		kiv_hal::TDisk_Address_Packet &dap = *reinterpret_cast<kiv_hal::TDisk_Address_Packet*>(context.rdi.r);
+
+		memcpy(&mDisk_Image[dap.lba_index*mBytes_Per_Sector], dap.sectors, mBytes_Per_Sector*dap.count);
+		Set_Status(context, kiv_hal::NDisk_Status::No_Error);
+	}
+}
---
+++ src/boot/keyboard.cpp
@@ -0,0 +1,59 @@
+#include "keyboard.h"
+#include <Windows.h>
+
+#include <iostream>
+
+HANDLE hConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
+bool Std_In_Redirected = GetFileType(hConsoleInput) != FILE_TYPE_CHAR;
+
+bool Init_Keyboard() {
+	//pokusime se vypnout echo na konzoli
+	//mj. na Win prestanou detekovat a "krast" napr. Ctrl+C
+
+	if (Std_In_Redirected) return true;	//neni co prepinat s presmerovanym vstupem
+		
+	DWORD mode;
+	bool echo_off = GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), &mode);
+	if (echo_off) echo_off = SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE), mode & (~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT)));
+	return echo_off;
+}
+
+bool Peek_Char() {
+	if (Std_In_Redirected) {		
+		return true;	//pokud je uz vstup uzavren, pak bude vzdy dostupny znak signalizujici konec vstupu/souboru
+	}
+
+	INPUT_RECORD record;
+	DWORD read;
+	
+	//zkontrolujeme prvni udalost ve frone, protoze jich tam muze byt vic a ruzneho typu
+	if (PeekConsoleInputA(hConsoleInput, &record, 1, &read) && (read > 0)) {
+		if (record.EventType == KEY_EVENT) 
+			return true;		
+	}
+
+	return false;
+}
+
+uint16_t Read_Char() {
+	char ch;
+	DWORD read;
+	
+	if (ReadFile(hConsoleInput, &ch, 1, &read, NULL))	//ReadConsoleA by neprecetlo presmerovany vstup ze souboru
+		return read > 0 ? ch : kiv_hal::NControl_Codes::NUL;
+		else return kiv_hal::NControl_Codes::EOT;	//chyba, patrne je zavrene vstupni handle
+	
+}
+
+
+void __stdcall Keyboard_Handler(kiv_hal::TRegisters &context) {
+	switch (static_cast<kiv_hal::NKeyboard>(context.rax.h)) {
+		case kiv_hal::NKeyboard::Peek_Char:	context.flags.non_zero = Peek_Char() ? 1 : 0;
+											break;
+
+		case kiv_hal::NKeyboard::Read_Char: context.rax.x = Read_Char();
+											break;
+
+		default: context.flags.carry = 1;			
+	}
+}
