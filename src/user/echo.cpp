1f6ba1634a592c7ad99c9555323c45bd4ef36262
Lenka Simeckova <simeckova.lenka@seznam.cz>
2018-11-08 10:05:06 +0100

    echo fix

--- src/user/echo.cpp
+++ src/user/echo.cpp
@@ -1,30 +1,36 @@
 #include "echo.h"
 #include "rtl.h"
+#include <stdlib.h>
 
 size_t __stdcall echo(const kiv_hal::TRegisters &regs) {
 
-	char* input = reinterpret_cast<char*>(regs.rdi.r);
+	const char* input = reinterpret_cast<char*>(regs.rdi.r);
 
+	char* first = "";
 	char* rest;
-	char* first = strtok_s(input, " ", &rest);
+	size_t inputsize = strlen(input) + 1;
+	char* tokenize = new char[inputsize];
+
+	strcpy_s(tokenize, strlen(tokenize), input);
+
+	first = strtok_s(tokenize, " ", &rest);
 
 	const kiv_os::THandle std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
 	size_t counter;
 	const char* notImplementedYet = "Not implemented yet.\n";
 
-	if (first != NULL && !strcmp(first, "on"))
+	if (first != NULL && !strcmp(first, "on") && (rest == NULL || !strcmp(rest, "")))
 	{
 		kiv_os_rtl::Write_File(std_out, notImplementedYet, strlen(notImplementedYet), counter);
 		kiv_os_rtl::Exit(1);
 		return 1;
 	}
 	
-	else if (first != NULL && !strcmp(first, "off"))
+	else if (first != NULL && !strcmp(first, "off") && (rest == NULL || !strcmp(rest, "")))
 	{
 		kiv_os_rtl::Write_File(std_out, notImplementedYet, strlen(notImplementedYet), counter);
 		kiv_os_rtl::Exit(1);
 		return 1;
-
 	}
 
 	else {
