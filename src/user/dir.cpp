60060eb280a3c82b116feb9111f10318ecbf6b92
Lenka Simeckova <simeckova.lenka@seznam.cz>
2018-11-13 16:32:18 +0100

    dir output

--- src/user/dir.cpp
+++ src/user/dir.cpp
@@ -26,12 +26,22 @@ size_t __stdcall dir(const kiv_hal::TRegisters &regs) {
 	kiv_os_rtl::Read_File(dirhandle, buffer, sizeof(buffer), counter);
 	buffer[counter] = '\0';
 
-	// TODO dodelat do hezkeho formatu
+	std::string text = "FILENAME\tRO\tHIDDEN\tSYSTEM\tVOLUME\tIS DIR\tARCHIVE\n";
 
-	std::string text;
 	for (size_t beg = 0; beg < counter; beg += sizeof(kiv_os::TDir_Entry)) {
+
 		const auto dir = reinterpret_cast<kiv_os::TDir_Entry*>(buffer + beg);
-		text.append(std::string(dir->file_name, sizeof kiv_os::TDir_Entry::file_name)).append("\n");
+		const auto file_attributes = static_cast<kiv_os::NFile_Attributes>(dir->file_attributes);
+
+		auto filename = std::string(dir->file_name, sizeof kiv_os::TDir_Entry::file_name);
+		auto read_only = std::to_string(dir->file_attributes & static_cast<uint8_t>(kiv_os::NFile_Attributes::Read_Only));
+		auto hidden = std::to_string(dir->file_attributes & static_cast<uint8_t>(kiv_os::NFile_Attributes::Hidden));
+		auto system_file = std::to_string(dir->file_attributes & static_cast<uint8_t>(kiv_os::NFile_Attributes::System_File));
+		auto volume_id = std::to_string(dir->file_attributes & static_cast<uint8_t>(kiv_os::NFile_Attributes::Volume_ID));
+		auto is_dir = std::to_string(dir->file_attributes & static_cast<uint8_t>(kiv_os::NFile_Attributes::Directory));
+		auto archive = std::to_string(dir->file_attributes & static_cast<uint8_t>(kiv_os::NFile_Attributes::Archive));
+
+		text.append(filename).append("\t").append(read_only).append("\t").append(hidden).append("\t").append(system_file).append("\t").append(volume_id).append("\t").append(is_dir).append("\t").append(archive).append("\n");
 	}
 
 	kiv_os_rtl::Write_File(std_out, text.c_str(), text.size(), counter);
