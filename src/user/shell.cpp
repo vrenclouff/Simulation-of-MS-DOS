b8ac188696c90a85dff6704382f19a7e0aba9cf3
Lukas Cerny <luc.cerny@gmail.com>
2018-11-20 12:38:21 +0100

    refactor TYPE

--- src/user/shell.cpp
+++ src/user/shell.cpp
@@ -4,17 +4,12 @@
 
 size_t __stdcall shell(const kiv_hal::TRegisters &regs) {
 
-	const kiv_os::THandle std_in = static_cast<kiv_os::THandle>(regs.rax.x);
-	const kiv_os::THandle std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
+	const auto std_in = static_cast<kiv_os::THandle>(regs.rax.x);
+	const auto std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
 
 	const size_t buffer_size = 256;
 	char buffer[buffer_size];
 	size_t read_counter = 0, write_counter = 0;
-	
-	//const char* intro = "Vitejte v kostre semestralni prace z KIV/OS.\n" \
-	//					"Prikaz exit ukonci shell.\n";
-	//kiv_os_rtl::Write_File(std_out, intro, strlen(intro), write_counter);
-
 
 	char prompt[100];
 	const char* exit = "exit";
--- src/user/type.cpp
+++ src/user/type.cpp
@@ -7,49 +7,39 @@
 
 size_t __stdcall type(const kiv_hal::TRegisters &regs) {
 
-	size_t written;
-	size_t read;
+	size_t wrote, read;
 	const kiv_os::THandle std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
 	const kiv_os::THandle std_in = static_cast<kiv_os::THandle>(regs.rax.x);
-	const char* linebreak = "\n";
 	   
-	auto input = std::string(reinterpret_cast<char*>(regs.rdi.r));
+	const auto input = std::string(reinterpret_cast<char*>(regs.rdi.r));
 
-	std::istringstream is(input);
 	std::string filename;
-	std::getline(is, filename, ' ');
+	std::getline(std::istringstream(input), filename, ' ');
 
 	if (!filename.empty()) {
 		kiv_os::THandle filehandle;
-		kiv_os_rtl::Open_File(input.c_str(), input.size(), filehandle, true, std::iostream::ios_base::in);
 
-		const kiv_os::THandle std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
+		if (!kiv_os_rtl::Open_File(input.c_str(), input.size(), filehandle, true, std::iostream::ios_base::in)) {
+			const auto error_msg = std::string_view("Cannot find the file.");
+			kiv_os_rtl::Write_File(std_out, error_msg.data(), error_msg.size(), wrote);
+			return 1;
+		}
 
-		char content[1124];
+		char content[2048];
 		do {
 			kiv_os_rtl::Read_File(filehandle, content, sizeof(content), read);
-			kiv_os_rtl::Write_File(std_out, content, read, written);
+			kiv_os_rtl::Write_File(std_out, content, read, wrote);
 		} while (read);
+
+		kiv_os_rtl::Write_File(std_out, "\n", 1, wrote);
+		kiv_os_rtl::Close_Handle(filehandle);
 	}
 	else {
-		const size_t buffer_size = 256;
-		char buffer[buffer_size];
-		size_t read;
-		std::vector<std::string> elements;
-		do
-		{
-			if (kiv_os_rtl::Read_File(std_in, buffer, buffer_size, read)) {
-				kiv_os_rtl::Write_File(std_out, linebreak, strlen(linebreak), written);
-				buffer[read] = 0;
-				elements.push_back(buffer);
-			}
-		} while (read != 0);
-
-		for (std::vector<std::string>::const_iterator i = elements.begin(); i != elements.end(); ++i)
-			std::cout << *i << linebreak;
+		const auto error_msg = std::string_view("Input is empty.\nUse: type <file_path>\n");
+		kiv_os_rtl::Write_File(std_out, error_msg.data(), error_msg.size(), wrote);
+		return 1;
 	}
 	
-
 	kiv_os_rtl::Exit(0);
 	return 0;
 }
