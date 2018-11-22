32cbf7f7830f008de49165662112e42bdcac102c
Lenka Simeckova <simeckova.lenka@seznam.cz>
2018-11-22 09:36:26 +0100

    echo refactoring

--- src/user/echo.cpp
+++ src/user/echo.cpp
@@ -1,43 +1,44 @@
 #include "echo.h"
 #include "rtl.h"
 
+#include <sstream>
+
 size_t __stdcall echo(const kiv_hal::TRegisters &regs) {
 
 	const char* input = reinterpret_cast<char*>(regs.rdi.r);
-
-	char* first = "";
-	char* rest;
-	size_t inputsize = strlen(input) + 1;
-	char* tokenize = new char[inputsize];
-
-	strcpy_s(tokenize, strlen(tokenize), input);
-
-	first = strtok_s(tokenize, " ", &rest);
-
 	const kiv_os::THandle std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
-	size_t counter;
-	const char* notImplementedYet = "Not implemented yet.\n";
 
-	if (first != NULL && !strcmp(first, "on") && (rest == NULL || !strcmp(rest, "")))
+	std::istringstream is(input);
+	std::string first;
+	std::string rest;
+	char space = ' ';
+
+	std::getline(is, first, space);
+	std::getline(is, rest);
+
+	size_t written;
+	const std::string notImplementedYet = "Not implemented yet.\n";
+
+	if ((!first.empty() && !first.compare("on")) && (rest.empty() || !rest.compare("")))
 	{
-		kiv_os_rtl::Write_File(std_out, notImplementedYet, strlen(notImplementedYet), counter);
+		kiv_os_rtl::Write_File(std_out, notImplementedYet.c_str(), notImplementedYet.length(), written);
 		kiv_os_rtl::Exit(1);
 		return 1;
 	}
 	
-	else if (first != NULL && !strcmp(first, "off") && (rest == NULL || !strcmp(rest, "")))
+	else if ((!first.empty() && !first.compare("off")) && (rest.empty() || !rest.compare("")))
 	{
-		kiv_os_rtl::Write_File(std_out, notImplementedYet, strlen(notImplementedYet), counter);
+		kiv_os_rtl::Write_File(std_out, notImplementedYet.c_str(), notImplementedYet.length(), written);
 		kiv_os_rtl::Exit(1);
 		return 1;
 	}
 
 	else {
-		kiv_os_rtl::Write_File(std_out, input, strlen(input), counter);
+		kiv_os_rtl::Write_File(std_out, input, strlen(input), written);
 	}
 
-	const char* linebreak = "\n";
-	kiv_os_rtl::Write_File(std_out, linebreak, strlen(linebreak), counter);
+	const std::string linebreak = "\n";
+	kiv_os_rtl::Write_File(std_out, linebreak.c_str(), linebreak.length(), written);
 
 	kiv_os_rtl::Exit(0);
 	return 0;
