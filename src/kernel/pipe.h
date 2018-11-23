f8ee2c466e1a15c69a447f56947e840ec3d061fc
Lenka Simeckova <simeckova.lenka@seznam.cz>
2018-11-24 00:52:53 +0100

    pipe fix

--- src/kernel/pipe.h
+++ src/kernel/pipe.h
@@ -10,7 +10,7 @@ public:
 		this->start = 0;
 		this->end = 0;
 		this->full = false;
-		this->writeSem = new Semaphore(0);
+		this->writeSem = new Semaphore(this->max);
 		this->readSem = new Semaphore(0);
 	}
 
