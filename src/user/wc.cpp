25416f5049888eaa8260c551e9cc201acc52a62a
Lenka Simeckova <simeckova.lenka@seznam.cz>
2018-11-28 00:37:03 +0100

    wc fix

--- src/user/wc.cpp
+++ src/user/wc.cpp
@@ -1,5 +1,6 @@
 #include "wc.h"
 #include "rtl.h"
+#include "error.h"
 
 #include <string>
 #include <sstream>
@@ -8,50 +9,52 @@
 
 size_t __stdcall wc(const kiv_hal::TRegisters &regs) {
 	std::string input = std::string(reinterpret_cast<char*>(regs.rdi.r));
+	const auto std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
+	const auto std_in = static_cast<kiv_os::THandle>(regs.rax.x);
 
 	std::istringstream is(input);
 	std::string first;
 	std::string second;
+	std::string arg;
 	char space = ' ';
 
 	std::getline(is, first, space);
 	std::getline(is, second, space);
+	std::getline(is, arg);
 
 	size_t written;
+	size_t read;
 	const std::string linebreak = "\n";
-	const kiv_os::THandle std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
 
-	if ((first.compare("\\v") || second.compare("\\c")) && (first.compare("\\c") || second.compare("\\v")))
+	if (first.compare("\\v") || second.compare("\\c\"\"") || !arg.empty())
 	{
-		const std::string incorrect_Use = "Incorrect use of find command. Try: find \\v \\c \"<some text or file>\"\n";
+		const std::string incorrect_Use = "The syntax of the command is incorrect.\n";
 		kiv_os_rtl::Write_File(std_out, incorrect_Use.c_str(), incorrect_Use.length(), written);
 		kiv_os_rtl::Exit(1);
 		return 1;
 	}
 
-	std::string arg;
-	std::getline(is, arg);
+	char buffer[1024];
+	std::stringstream stream;
+	do {
+		if (!kiv_os_rtl::Read_File(std_in, buffer, sizeof buffer, read)) {
+			const kiv_os::NOS_Error error = kiv_os_rtl::Last_Error;
+			const auto error_msg = Error_Message(error);
+			kiv_os_rtl::Write_File(std_out, error_msg.data(), error_msg.length(), written);
+			const auto error_code = static_cast<uint16_t>(error);
+			kiv_os_rtl::Exit(error_code);
+			return error_code;
+		}
+		stream << std::string(buffer, read);
+	} while (read);
+	
+	std::istream_iterator<std::string> begin(stream);
+	std::istream_iterator<std::string> end;
 
-	std::istringstream iswords(arg);
-	std::string skip;
-	char quote = '"';
-	std::getline(std::getline(iswords, skip, quote), arg, quote);
+	std::vector<std::string> elements(begin, end);
 
-	std::string output;
-	if (!arg.empty())
-	{
-		std::stringstream stream(arg);
-		std::istream_iterator<std::string> begin(stream);
-		std::istream_iterator<std::string> end;
-
-		std::vector<std::string> elements(begin, end);
-
-		std::string wordcount = std::to_string(elements.size());
-		output = wordcount;
-	}
-	else {
-		output = "0";
-	}
+	std::string wordcount = std::to_string(elements.size());
+	std::string output = wordcount;
 
 	kiv_os_rtl::Write_File(std_out, output.c_str(), output.length(), written);
 	kiv_os_rtl::Write_File(std_out, linebreak.c_str(), linebreak.length(), written);
