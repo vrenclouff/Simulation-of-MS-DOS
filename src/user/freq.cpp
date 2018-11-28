7601db852e532a94c12f71614e0d43c6541f13c5
Lukas Cerny <luc.cerny@gmail.com>
2018-11-28 09:54:59 +0100

    refactored FREQ

--- src/user/freq.cpp
+++ src/user/freq.cpp
@@ -1,40 +1,42 @@
 #include "freq.h"
 #include "rtl.h"
+#include "error.h"
 
 #include <map>
-#include <sstream>
 
 size_t __stdcall freq(const kiv_hal::TRegisters &regs) {
 
 	const auto std_out = static_cast<kiv_os::THandle>(regs.rbx.x);
 	const auto std_in = static_cast<kiv_os::THandle>(regs.rax.x);
 
-	//const auto input = reinterpret_cast<char*>(regs.rdi.r);
-
-	std::stringstream ss;
+	std::map<unsigned char, int> frequencies;
 	char buffer[1024];
 	size_t read, written;
-	do
-	{
+	do {
 		if (kiv_os_rtl::Read_File(std_in, buffer, sizeof buffer, read)) {
-			kiv_os_rtl::Write_File(std_out, "\n", 1, written);
-			buffer[read] = 0;
-			ss << buffer;
+			for (auto i = 0; i < read; i++) {
+				frequencies[buffer[i]]++;
+			}
+		}
+		else {
+			const kiv_os::NOS_Error error = kiv_os_rtl::Last_Error;
+			const auto error_msg = Error_Message(error);
+			const auto error_code = static_cast<uint16_t>(error);
+
+			kiv_os_rtl::Write_File(std_out, error_msg.c_str(), error_msg.length(), written);
+			kiv_os_rtl::Exit(error_code);
+			return error_code;
 		}
 	} while (read);
 
-	const auto bytes = ss.str();
-	std::map<unsigned char, int> frequencies;
-	for (int i = 0; i < bytes.size(); i++) {
-		frequencies[bytes[i]]++;
-	}
-
 	char out[255];
 	for (const auto& item : frequencies) {
-		sprintf_s(out, sizeof out, "0x%hhx : %d\n", item.first, item.second);
+		sprintf_s(out, sizeof out, "\n0x%hhx : %d", item.first, item.second);
 		kiv_os_rtl::Write_File(std_out, out, strlen(out), written);
 	}
 	
+	kiv_os_rtl::Write_File(std_out, "\n", 1, written);
+
 	kiv_os_rtl::Exit(0);
 	return 0;
 }
